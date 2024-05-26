b02f8d16b5e4a5197d559be2a26f713b8233a919
VKongkin <voeun_kongkin@pp.bbu.edu.kh>
2024-05-26 22:33:56 +0700

    update for google run

--- src/main/java/kongkin/bbu/edu/webapi/WebApiApplication.java
+++ src/main/java/kongkin/bbu/edu/webapi/WebApiApplication.java
@@ -2,10 +2,16 @@ package kongkin.bbu.edu.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class WebApiApplication {
-
+	@GetMapping("")
+	public String greeting(){
+		return "Welcome to web api";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(WebApiApplication.class, args);
 	}
